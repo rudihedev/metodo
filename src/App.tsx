24f001b17700b88f1168f1dbc27e2c1231b6da1f
rudihedev <rudihedev@gmail.com>
2025-11-10 20:30:04 +0700

    revise app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Profile } from "./profile";
+
 export function App() {
   return (
     <div>
       <h1>METODO: WHAT TO DO TODAY?</h1>
+      <Profile />
+      <Profile />
+      <Profile />
     </div>
   );
 }
