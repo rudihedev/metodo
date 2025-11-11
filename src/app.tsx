c3cce7890b281233f1cbee70d87e74dc1da668ee
rudihedev <rudihedev@gmail.com>
2025-11-11 08:22:11 +0700

    feat: profile.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,14 @@ export function App() {
   return (
     <div>
       <h1>METODO: WHAT TO DO TODAY?</h1>
-      <Profile />
-      <Profile />
-      <Profile />
+      <Profile
+        imageUrl="https://i.imgur.com/MK3eW3Am.jpg"
+        name="Katherine Johnson"
+      />
+      <Profile
+        imageUrl="https://thediffpodcast.com/assets/images/jordan-2c43ff762e625d49ea58424e3e74188a.jpg"
+        name="Jordan Walke"
+      />
     </div>
   );
 }
