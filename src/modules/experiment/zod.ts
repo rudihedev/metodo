fc3bbc2945ef21352b715e483141f7f65b8dce6e
rudihedev <rudihedev@gmail.com>
2025-11-22 13:10:18 +0700

    revise zod.ts

--- src/modules/experiment/zod.ts
+++ src/modules/experiment/zod.ts
@@ -1,6 +1,13 @@
 import * as z from "zod";
 
-const Player = z.object({
-  username: z.string(),
-  xp: z.number(),
+const TaskSchema = z.object({
+  id: z.number().positive(),
+  title: z.string().min(1).max(100),
+  isDone: z.boolean(),
+});
+
+TaskSchema.parse({
+  id: 1,
+  title: "Learn",
+  isDone: true,
 });
