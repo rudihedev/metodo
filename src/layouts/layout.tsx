7d8bf5ef91629f29130ffb144530fa4d92bc5c70
rudihedev <rudihedev@gmail.com>
2025-11-29 17:54:33 +0700

    feat: navbar in layout

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,10 +1,22 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 export function Layout() {
   return (
     <div>
-      <h1>Layout</h1>
+      <nav className="flex items-center justify-between gap-4">
+        <h1 className="mt-2 text-center text-3xl font-bold text-green-900">
+          <Link to="/">METODO</Link>
+        </h1>
 
+        <ul className="inline-flex gap-4">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
       <Outlet />
     </div>
   );
