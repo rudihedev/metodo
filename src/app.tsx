feca622ecbfc9f522818f945dbe7187b7466d7e0
rudihedev <rudihedev@gmail.com>
2025-11-29 17:32:17 +0700

    feat: navbar in app route

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,25 @@
 import { Tasks } from "@/modules/task/tasks";
+import { Link } from "react-router";
 
 export function App() {
   return (
     <div className="flex justify-center">
       <main className="w-full max-w-lg space-y-2">
-        <h1 className="mt-2 text-center text-3xl font-bold text-green-900">
-          METODO
-        </h1>
-        <h2 className="text-center text-2xl font-bold text-green-700">
-          📋 What to Do Today? 🔍
-        </h2>
+        <nav className="flex items-center justify-between gap-4">
+          <h1 className="mt-2 text-center text-3xl font-bold text-green-900">
+            <Link to="/">METODO</Link>
+          </h1>
+
+          <ul className="inline-flex gap-4">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
         <Tasks />
       </main>
     </div>
