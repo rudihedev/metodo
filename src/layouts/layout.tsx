016c845bd4e3fa773f8a27c842767724a8ef57c9
rudihedev <rudihedev@gmail.com>
2025-11-30 08:45:22 +0700

    fix: layout style

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -2,22 +2,24 @@ import { Link, Outlet } from "react-router";
 
 export function Layout() {
   return (
-    <div>
-      <nav className="flex items-center justify-between gap-4">
-        <h1 className="mt-2 text-center text-3xl font-bold text-green-900">
-          <Link to="/">METODO</Link>
-        </h1>
+    <div className="flex justify-center">
+      <div className="w-full max-w-lg">
+        <nav className="flex items-center justify-between gap-4">
+          <h1 className="mt-2 mb-4 text-center text-3xl font-bold text-green-900">
+            <Link to="/">METODO</Link>
+          </h1>
 
-        <ul className="inline-flex gap-4">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
+          <ul className="inline-flex gap-4">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Outlet />
+      </div>
     </div>
   );
 }
